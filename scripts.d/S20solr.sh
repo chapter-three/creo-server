b5e39aa147454207fae74a29a719e2ac8f0b2fef
Brad Erickson <eosrei@gmail.com>
2012-02-07 20:31:35 -0800

    Missing some $

--- scripts.d/S20solr.sh
+++ scripts.d/S20solr.sh
@@ -3,8 +3,8 @@ case "$COMMAND" in
   create_solr)
     set_message "Creating solr instance..."
     cp -ar $SOLR_DATA_DIR/$TEMPLATE $SOLR_DATA_DIR/$PROJECT
-    cp -a TOMCAT_LOCALHOST_DIR/${TEMPLATE}.xml TOMCAT_LOCALHOST_DIR/$PROJECT.xml
-    sed -i "s/$TEMPLATE/$PROJECT/" TOMCAT_LOCALHOST_DIR/$PROJECT.xml
+    cp -a $TOMCAT_LOCALHOST_DIR/$TEMPLATE.xml $TOMCAT_LOCALHOST_DIR/$PROJECT.xml
+    sed -i "s/$TEMPLATE/$PROJECT/" $TOMCAT_LOCALHOST_DIR/$PROJECT.xml
     cp -ar $TOMCAT_WEBAPP_DIR/$TEMPLATE $TOMCAT_WEBAPP_DIR/$PROJECT
     set_message "Restarting Tomcat..."
     $TOMCAT_SERVICE_PATH restart
@@ -18,7 +18,7 @@ case "$COMMAND" in
     set_message "Removing solr instance..."
     rm -r $SOLR_DATA_DIR/$PROJECT
     rm $TOMCAT_LOCALHOST_DIR/$PROJECT.xml
-    rm -r TOMCAT_WEBAPP_DIR/$PROJECT
+    rm -r $TOMCAT_WEBAPP_DIR/$PROJECT
     # @todo: Make sure files were deleted before the restart is done.
     set_message "Restarting Tomcat..."
     $TOMCAT_SERVICE_PATH restart
