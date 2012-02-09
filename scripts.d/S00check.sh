cb49f1c7c8c480900165f4851ff833b94aa60030
Brad Erickson <eosrei@gmail.com>
2012-02-08 17:31:05 -0800

    Create error checking script

---
+++ scripts.d/S00check.sh
@@ -0,0 +1,15 @@
+# Error check command options
+
+case "$COMMAND" in
+  create)
+    if [ ! -d $WWW_DIR/$TEMPLATE ] ; then
+      set_message "Template $TEMPLATE does not exist in $WWW_DIR" error
+      exit 1
+    fi
+    if [ -d $WWW_DIR/$PROJECT ] ; then
+      set_message "Project $PROJECT already exists in $WWW_DIR" error
+      exit 1
+    fi
+  ;;
+esac
+
