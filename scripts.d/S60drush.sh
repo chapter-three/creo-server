a4b48df4fb473c75c759287f971d7baa151b6a37
Brad <eosrei@gmail.com>
2012-12-13 23:00:37 +0000

    Change backup location directory

--- scripts.d/S60drush.sh
+++ scripts.d/S60drush.sh
@@ -5,8 +5,7 @@ case "$COMMAND" in
     #Method to list all files: ls -1 -d /var/www/projects/*
     #Method to show last git update : git log -1 --format=%cd --date=iso
     mkdir -p $BACKUP_DIR/$PROJECT
-    NOW=$(date +"%m-%d-%Y")
-    drush archive-backup -r $WWW_DIR/$PROJECT --generator "Creo Project Manager"  --destination=$BACKUP_DIR/$PROJECT/$PROJECT-$NOW.tar.gz  --overwrite
+    drush archive-backup -r $WWW_DIR/$PROJECT --generator "Creo Project Manager"  --destination=$BACKUP_DIR/$PROJECT-$DATESTAMP.tar.gz  --overwrite
   ;;
 
   create | import)
