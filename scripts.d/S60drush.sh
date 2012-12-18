aff6a92d164d031998c29990178136adeb96d87f
Brad <eosrei@gmail.com>
2012-12-18 01:35:11 +0000

    Split backup archives larger than 4GB

--- scripts.d/S60drush.sh
+++ scripts.d/S60drush.sh
@@ -4,8 +4,14 @@ case "$COMMAND" in
     #Backup site using archive
     #Method to list all files: ls -1 -d /var/www/projects/*
     #Method to show last git update : git log -1 --format=%cd --date=iso
-    mkdir -p $BACKUP_DIR/$PROJECT
+    mkdir -p $BACKUP_DIR
     drush archive-backup -r $WWW_DIR/$PROJECT --generator "Creo Project Manager"  --destination=$BACKUP_DIR/$PROJECT-$DATESTAMP.tar.gz  --overwrite
+    filesize=$(stat --format="%s" $BACKUP_DIR/$PROJECT-$DATESTAMP.tar.gz)
+    # If file size greater than 4GB, split and delete original
+    if [ $filesize > 4294967296 ]; then
+      split -b 4G -d $BACKUP_DIR/$PROJECT-$DATESTAMP.tar.gz $BACKUP_DIR/$PROJECT-$DATESTAMP.tar.gz.split_
+      rm $BACKUP_DIR/$PROJECT-$DATESTAMP.tar.gz
+    fi
   ;;
 
   create | import)
